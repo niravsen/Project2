7bf5d2dea33eca574b5e3097b6f269a61f3d86be
kkb <k-bhasin@hotmail.com>
2018-09-19 16:29:38 -0400

    To add bcrypt functionality

--- models/user.js
+++ models/user.js
@@ -1,10 +1,23 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
     age: DataTypes.INTEGER,
     country: DataTypes.STRING,
     progress: DataTypes.INTEGER
-  });
+  }, {
+      freezeTableName: true,
+      instanceMethods: {
+        generateHash(password) {
+          return bcrypt.hash(password, bcrypt.genSaltSync(8));
+        },
+        validPassword(password) {
+          return bcrypt.compare(password, this.password);
+        }
+      }
+    });
+
   return User;
-};
+}
